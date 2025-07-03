de325546f2a0e0ead7e62c063baa1b595bb5a24c
Ali Raza <aliraza21112005@gmail.com>
2025-07-04 00:51:02 +0500

    Extracing data from the config File

--- Day7(Selenium Automatic TODO list)/src/day7/__init__.py
+++ Day7(Selenium Automatic TODO list)/src/day7/__init__.py
@@ -1,2 +1,31 @@
+import time
+import os
+import json
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from datetime import date
+
+DRIVER_PATH = "chromedriver.exe"
+OP = webdriver.ChromeOptions()
+OP.add_argument('headless')
+DRIVER = webdriver.Chrome()
+
+def login():
+    with open('config.json') as config_file:
+        credientials = json.load(config_file)
+   
 def main() -> None:
-    print("Hello from day7!")
+    try:
+        DRIVER.get("https://trello.com/")
+        input("BOT operation completed. Press enter to exit.")
+        DRIVER.close()
+    except Exception as e:
+        print(e)
+        DRIVER.close()
+    
+
+if __name__ == '__main__':
+    main()

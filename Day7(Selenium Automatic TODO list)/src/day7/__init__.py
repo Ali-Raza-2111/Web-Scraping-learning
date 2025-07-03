0c7ab0411fc5462e9f4b92078478204699c9b109
Ali Raza <aliraza21112005@gmail.com>
2025-07-04 01:32:12 +0500

    Login into the website Successfully

--- Day7(Selenium Automatic TODO list)/src/day7/__init__.py
+++ Day7(Selenium Automatic TODO list)/src/day7/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import time
 import os
 import json
@@ -17,10 +16,23 @@ DRIVER = webdriver.Chrome()
 def login():
     with open('config.json') as config_file:
         credientials = json.load(config_file)
-   
+        time.sleep(2)
+        DRIVER.find_element(By.XPATH, value = "//a[@href='https://id.atlassian.com/login?application=trello&continue=https%3A%2F%2Ftrello.com%2Fauth%2Fatlassian%2Fcallback%3Fdisplay%3DeyJ2ZXJpZmljYXRpb25TdHJhdGVneSI6InNvZnQifQ%253D%253D&display=eyJ2ZXJpZmljYXRpb25TdHJhdGVneSI6InNvZnQifQ%3D%3D']").click()
+        time.sleep(5)
+        Email = DRIVER.find_element(By.CSS_SELECTOR, "input[type='email']")
+        time.sleep(2)
+        Email.send_keys(credientials['Username'])
+        time.sleep(2)
+        DRIVER.find_element(By.CSS_SELECTOR, "span[class='css-178ag6o']").click()
+        time.sleep(3)
+        password = DRIVER.find_element(By.CSS_SELECTOR, "input[id='password']")
+        password.send_keys(credientials['Password'])
+        time.sleep(2)
+        DRIVER.find_element(By.CSS_SELECTOR, "span[class='css-178ag6o']").click()
 def main() -> None:
     try:
         DRIVER.get("https://trello.com/")
+        login()
         input("BOT operation completed. Press enter to exit.")
         DRIVER.close()
     except Exception as e:
